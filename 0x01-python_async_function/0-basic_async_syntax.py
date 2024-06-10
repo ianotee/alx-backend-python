539bf3f2e90f9293c791b0d1793751a6fd640220
ianotee <oteeian500@gmail.com>
2024-06-10 12:11:59 +0000

    first commit

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Write an asynchronous coroutine that takes in an integer.
+"""
+
+
+import random
+import asyncio
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """Wait for some time"""
+    wait_time = random.random() * max_delay
+    await asyncio.sleep(wait_time)
+    return wait_time
